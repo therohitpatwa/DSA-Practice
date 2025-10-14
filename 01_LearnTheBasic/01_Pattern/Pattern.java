d2d118c8309693e5ee755a973b84d135efe3c478
Rohit Patwa <therohitpatwa@gmail.com>
2025-10-14 16:04:29 +0530

    Practicng..

--- 01_LearnTheBasic/01_Pattern/Pattern.java
+++ 01_LearnTheBasic/01_Pattern/Pattern.java
@@ -379,11 +379,31 @@ class Pattern {
          }
        
     }
+
+      public static void pattern22(int n) {
+         for(int i=0;i<2*n-1;i++)
+         {
+             for(int j=0;j<2*n-1;j++)
+             {
+                int top=i;
+                int bottom=(2*n-2)-i;
+                int left=j;
+                int right=(2*n-2)-j;
+
+
+                System.out.print(n-Math.min(Math.min(top, bottom),Math.min(left, right))+" ");
+
+             }
+             System.err.println(" ");
+         }
+       
+    }
+    
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        pattern21(n);
+        pattern22(n);
         sc.close();
 
     }

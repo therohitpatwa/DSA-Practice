ad76a1f4e82fa5e483176b6e907d4d707766749e
Rohit Patwa <therohitpatwa@gmail.com>
2026-01-20 04:03:38 +0530

    Practice...

--- 02_Sorting/BinarySearch.java
+++ 02_Sorting/BinarySearch.java
@@ -40,38 +40,38 @@
 
 
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class BinarySearch
-{
+// class BinarySearch
+// {
 
-   public static void main(String[] args) {
-     Scanner sc=new Scanner(System.in);
-     int n=sc.nextInt();
-     int arr[]=new int[n];
-     for(int i=0;i<n;i++)
-     {
-        arr[i]=sc.nextInt();
-     }
-     System.out.println( BS(arr,0,arr.length-1, 14));
-   }
-   public static int BS(int[] arr,int start,int end,int target)
-   {
-       if(start>end)
-       return -1;
+//    public static void main(String[] args) {
+//      Scanner sc=new Scanner(System.in);
+//      int n=sc.nextInt();
+//      int arr[]=new int[n];
+//      for(int i=0;i<n;i++)
+//      {
+//         arr[i]=sc.nextInt();
+//      }
+//      System.out.println( BS(arr,0,arr.length-1, 14));
+//    }
+//    public static int BS(int[] arr,int start,int end,int target)
+//    {
+//        if(start>end)
+//        return -1;
 
     
-       int mid = start + (end - start) / 2;
+//        int mid = start + (end - start) / 2;
 
 
-       if(arr[mid]==target)
-       return mid;
-       if(arr[mid]>target)
-       return BS(arr, start, mid-1, target);
-       else
-       return BS(arr, mid+1, end, target);
+//        if(arr[mid]==target)
+//        return mid;
+//        if(arr[mid]>target)
+//        return BS(arr, start, mid-1, target);
+//        else
+//        return BS(arr, mid+1, end, target);
       
    
-   }
+//    }
     
-}
+// }
